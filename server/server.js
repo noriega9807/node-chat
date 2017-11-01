1b680c5a5043f329464309e10cca855859d5c6b1
Erick Noriega <noriega0893@gmail.com>
2017-11-01 13:50:55 -0600

    Greet new user, and alert others

--- server/server.js
+++ server/server.js
@@ -20,14 +20,32 @@ io.on('connection', (socket) => {
         console.log('User disconnected from server');
     });
 
+    socket.emit('newMessage', {
+        from: 'Admin',
+        text: 'Welcome to the chat app',
+        createdAt: new Date().getTime()
+    });
+
+    socket.broadcast.emit('newMessage', {
+        from: 'Admin',
+        text: 'New user joined',
+        createdAt: new Date().getTime()
+    });
+
     socket.on('createMessage', (message) => {
         console.log('createMessage', message);
+
         io.emit('newMessage', {
             from: message.from,
             text: message.text,
             createdAt: new Date().getTime()
         });
 
+        // socket.broadcast.emit('newMessage', {
+        //     from: message.from,
+        //     text: message.text,
+        //     createdAt: new Date().getTime()
+        // });
     });
 });
 
